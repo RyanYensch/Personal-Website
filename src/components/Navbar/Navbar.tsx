da1e4ece62d64978dba672d155b7f21363a03586
Ryan Yensch <ry2005@outlook.com>
2026-02-26 13:50:31 +1100

    added simple navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
             <div className="navbar-container">
                 <ul className="navbar-links">
                     <li className="navbar-item">
-                        <Link className="navbar-link" to="/">
+                        <Link className="navbar-item-link" to="/">
                             Home
                         </Link>
                     </li>
