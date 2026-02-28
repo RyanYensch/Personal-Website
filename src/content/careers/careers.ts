704dc81cc7fc6290f50ea79eacaa8674f2f11a52
Ryan Yensch <ry2005@outlook.com>
2026-03-01 01:04:28 +1100

    updated careers

--- src/content/careers/careers.ts
+++ src/content/careers/careers.ts
@@ -9,7 +9,17 @@ export const Careers: Career[] = [
         end: "Present",
         summary: "Coordinating and managing the eReuse volunteer group, which focuses on refurbishing donated technology for those who need it.",
         coverImage: "/ereuse-logo.png",
-        tags: ["Leadership", "Technology", "Hardware", "Organisation", "Teaching"],
+        tags: ["Leadership", "Technology", "Hardware", "Organisation", "Teaching", "Communication"],
         points: ["Lead Senior and General Volunteer Skills", "Managed communications to various sources."]
+    },
+    {
+        title: "Lead Coding Instructor",
+        employer: "CODE4FUN",
+        start: "October 2024",
+        end: "Present",
+        summary: "Currently working as a tutor teaching classrooms of around 15-30 primary and high school students coding fundamentals and providing one-on-one classroom support with their programming projects and classwork. Teaching Scratch, Java, and Python, and encouraging a passion for computer science.",
+        coverImage: "/code4fun.png",
+        tags: ["Technology", "Teaching", "Communication", "Tutoring"],
+        points: ["Teaching children of various ages between Grade 12 - Grade 12", "Instruct and teach programming skills in Java, Python, and Scratch", "Instruct classes of 15-30 students in-person, and online"]
     }
 ]
